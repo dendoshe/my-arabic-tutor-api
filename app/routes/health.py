08188c1ccd17c658fe50b8904485d3065fc59d64
Diana Jumaili <44396597+dendoshe@users.noreply.github.com>
2025-06-06 12:24:12 +0200

    removed main block

--- app/routes/health.py
+++ app/routes/health.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/health")
 async def health_check():
     return {"status": "ok", "message": "Service is up and running"}
